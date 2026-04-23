ed0d8d3f9582884706101c87eff6d0645e0dd8a6
Sam Dunlap <65867570+Radishmouse22@users.noreply.github.com>
2026-04-22 21:10:37 -0500

    added the rest of the models, sample data, and made the admin page the primary page

--- django-project/models/admin.py
+++ django-project/models/admin.py
@@ -1,7 +1,45 @@
 from django.contrib import admin
+from django.db.models.fields.related import ForeignObjectRel
 from djangoql.admin import DjangoQLSearchMixin
-from .models import Customer
+from .models import (
+    Customer, Employee, Vehicle, Billing,
+    Sales, ServiceData, WorkedOn, InventoryVehicle
+)
+
+def get_fields(model):
+    return [
+        field.name for field in model._meta.get_fields()
+        if not isinstance(field, ForeignObjectRel)
+    ]
 
 @admin.register(Customer)
 class CustomerAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
-    list_display = ('name', 'address', 'email', 'phoneNumber', 'customerType')
+    list_display = get_fields(Customer)
+
+@admin.register(Employee)
+class EmployeeAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
+    list_display = get_fields(Employee)
+
+@admin.register(Vehicle)
+class VehicleAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
+    list_display = get_fields(Vehicle)
+
+@admin.register(Billing)
+class BillingAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
+    list_display = get_fields(Billing)
+
+@admin.register(Sales)
+class SalesAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
+    list_display = get_fields(Sales)
+
+@admin.register(ServiceData)
+class ServiceDataAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
+    list_display = get_fields(ServiceData)
+
+@admin.register(WorkedOn)
+class WorkedOnAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
+    list_display = get_fields(WorkedOn)
+
+@admin.register(InventoryVehicle)
+class InventoryVehicleAdmin(DjangoQLSearchMixin, admin.ModelAdmin):
+    list_display = get_fields(InventoryVehicle)
--- django-project/models/models.py
+++ django-project/models/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.contrib.auth.models import User
 
 class Customer(models.Model):
     name = models.CharField(max_length=60)
@@ -16,3 +19,106 @@ class Customer(models.Model):
         choices=CustomerType.choices,
         default=CustomerType.SERVICE,
     )
+
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    address = models.CharField(max_length=255)
+    phoneNumber = models.CharField(max_length=10)
+
+
+class Vehicle(models.Model):
+    make = models.CharField(max_length=60)
+    model = models.CharField(max_length=60)
+    year = models.IntegerField()
+    vin = models.CharField(max_length=17, unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class VehicleStatus(models.TextChoices):
+        INVENTORY = 'Inventory'
+        SOLD = 'Sold'
+        IN_SERVICE = 'In-Service'
+
+    status = models.CharField(
+        max_length=10,
+        choices=VehicleStatus.choices,
+        default=VehicleStatus.INVENTORY,
+    )
+
+
+class Billing(models.Model):
+    date = models.DateField()
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+    class PaymentMethod(models.TextChoices):
+        CASH = 'Cash'
+        CREDIT = 'Credit'
+        DEBIT = 'Debit'
+        FINANCING = 'Financing'
+        CHECK = 'Check'
+
+    paymentMethod = models.CharField(
+        max_length=9,
+        choices=PaymentMethod.choices,
+    )
+
+
+class Sales(models.Model):
+    vehicle = models.OneToOneField(Vehicle, on_delete=models.PROTECT) # uses OneToOneField since a vehicle should only ever be sold once.
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    dateSold = models.DateField()
+    tagNumber = models.CharField(max_length=20, unique=True)
+    customizations = models.TextField(blank=True)
+    billing = models.ForeignKey(Billing, on_delete=models.PROTECT)
+
+
+class ServiceData(models.Model):
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.PROTECT)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    arrivalMileage = models.IntegerField()
+    departureMileage = models.IntegerField()
+    estimate = models.DecimalField(max_digits=10, decimal_places=2)
+    billing = models.ForeignKey(Billing, on_delete=models.PROTECT)
+    note = models.TextField(blank=True)
+
+
+class WorkedOn(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.PROTECT)
+    serviceData = models.ForeignKey(ServiceData, on_delete=models.PROTECT)
+    hours = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class InventoryVehicle(models.Model):
+    vehicle = models.OneToOneField(Vehicle, on_delete=models.PROTECT) # uses OneToOneField since a vehicle should only ever be sold once.
+
+    class Location(models.TextChoices):
+        MAIN_LOT = 'Main Lot'
+        REMOTE_LOT = 'Remote Lot'
+        OTHER_DEALERSHIP = 'Other Dealership'
+        IN_TRANSIT = 'In Transit'
+
+    location = models.CharField(
+        max_length=17,
+        choices=Location.choices,
+        default=Location.MAIN_LOT,
+    )
+    note = models.TextField(blank=True)
+
+
+# when an inventoryVehicle is created, set the corresponding to be in inventory
+@receiver(post_save, sender=InventoryVehicle)
+def sync_vehicle_status_on_save(sender, instance, **kwargs):
+    instance.vehicle.status = Vehicle.VehicleStatus.INVENTORY
+    instance.vehicle.save()
+
+# likewise for when an inventory vehicle is created
+@receiver(post_delete, sender=InventoryVehicle)
+def sync_vehicle_status_on_delete(sender, instance, **kwargs):
+    instance.vehicle.status = Vehicle.VehicleStatus.IN_SERVICE
+    instance.vehicle.save()
+
+# when a sales record is created, set the corresponding vehicle to sold
+@receiver(post_save, sender=Sales)
+def sync_vehicle_status_on_sale(sender, instance, **kwargs):
+    instance.vehicle.status = Vehicle.VehicleStatus.SOLD
+    instance.vehicle.save()
